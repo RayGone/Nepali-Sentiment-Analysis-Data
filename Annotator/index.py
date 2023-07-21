cb59873eb39158ca0cd41dff52cdb0b95aea3e2b
Regan <gregatom99@gmail.com>
2023-07-21 14:30:08 +1000

    Data Access From DataFrame

--- Annotator/index.py
+++ Annotator/index.py
@@ -22,7 +22,6 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 data = pd.read_csv(file_path)
 data.index = range(data.shape[0])
-print(data.shape)
 
 if 'label' not in data:
     data['label'] = None
@@ -42,14 +41,13 @@ def getItem():
     unlabeled = data[data['label_id'] == -1]
     if not unlabeled.empty:
         index = random.choice(unlabeled.index)
-        return {"data":dict(data.loc[index]),'index':int(index),'remaining':int(unlabeled.shape[0])}
+        return {"data":data.loc[index].to_dict(),'index':int(index),'remaining':int(unlabeled.shape[0])}
     else:
         return {'error':'No more'}
 
 
 @app.route("/set_label/<index>/<label>")
 def setItem(index,label):
-    
     index = int(index)
     label = int(label)
     data.at[index,'label_id'] = label
@@ -60,3 +58,4 @@ def setItem(index,label):
 
 if __name__ == '__main__':
     app.run()
+    pass
